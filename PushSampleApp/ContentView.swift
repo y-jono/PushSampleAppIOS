066709d47f5e27c708198bf722a5ec56299b2f7b
y_jono <pek9dkl@gmail.com>
2020-11-25 23:23:36 +0900

    Initial Commit

---
+++ PushSampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushSampleApp
+//
+//  Created by Seino Yoshinori on 2020/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
